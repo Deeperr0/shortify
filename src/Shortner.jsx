2ccb364bbacb51ee18706ff6a3b1a5e7d1555b6c
Yousef Attia <60562288+Deeperr0@users.noreply.github.com>
2024-09-23 15:11:10 +0200

    Added Error handling

--- src/Shortner.jsx
+++ src/Shortner.jsx
@@ -5,15 +5,30 @@ export default function Shortner(props) {
 	const windowWidth = props.windowWidth;
 
 	async function shortenLink(event) {
-		event.preventDefault();
-
+	    event.preventDefault();
+	
+	    try {
 		const url = document.getElementById("url").value;
+		if (!url) {
+		    throw new Error("URL cannot be empty");
+		}
+	
 		const response = await axios.get(
-			`https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`
+		    `https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`
 		);
-		localStorage.setItem(url, response.data.shorturl);
-		window.dispatchEvent(new Event("storageChange"));
+		
+		if (response.data.shorturl) {
+		    localStorage.setItem(url, response.data.shorturl);
+		    window.dispatchEvent(new Event("storageChange"));
+		} else {
+		    throw new Error("Failed to shorten the URL.");
+		}
+	    } catch (error) {
+		console.error("Error shortening the URL:", error.message);
+		alert("An error occurred while shortening the URL. Please try again.");
+	    }
 	}
+
 
 	return (
 		<div className={windowWidth >= 811 ? "shortner" : "shortner-mobile"}>
